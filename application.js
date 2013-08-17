31efff8909b3d498c1d29be018ff2b0c2bc9c57d
Evgeniya Kostenko <erunda_88@mail.ru>
2013-08-17 19:12:58 +0400

    add hide question feature

--- application.js
+++ application.js
@@ -3,7 +3,7 @@
 function IsEmail(email) {
   var regex = /^([a-zA-Z0-9_.+-])+\@(([a-zA-Z0-9-])+\.)+([a-zA-Z0-9]{2,4})+$/;
   return regex.test(email);
-};
+}
 function hideAllErrors() {
   $('.worksheet__question__question__input__error').hide();
 }
@@ -32,31 +32,17 @@ function validateMyForm(){
   } else {
     for (var i = 0; i < errors.length; ++i) {
       $("[name='" + errors[i] +"']").next('.worksheet__question__question__input__error').show();
-    };
+    }
     return false;
-  };
-};
+  }
+}
 
-  
-  //$("form.worksheet").bind('submit',function(){
+function hideQuestion(element) { 
+  $(element).siblings(':not(.worksheet__question__number):not(.worksheet__question__question__input__error)').slideToggle(300);
+}
 
-/*  if (($("input[name='answer[4]']").val() >= 1900) && (($("input[name='answer[4]']").val() <= 2020))) {
-        if ($("input[name='Moi_krug_lnk']").val().indexOf("moikrug.ru") != -1) {
-          if (IsEmail($("input[name='email']").val())) {
-            if ($("input[name='personal_data']").val()) {
-              return true;
-            } else {
-              $(".worksheet__question__question__input__error"[name='personal_data']).show();
-            }
-          } else {
-            $(".worksheet__question__question__input__error"[name='email']).show();
-          }
-        } else {
-          $(".worksheet__question__question__input__error"[name='Moi_krug_lnk']).show();
-        }
-      } else {
-        $(".worksheet__question__question__input__error"[name='answer[4]']).show();
-      }
-    } else {
-      $(".worksheet__question__question__input__error"[name='answer[1]']).show();
-    }*/
+$(document).ready(function init () {
+  $('.worksheet__question__number').click(function () {
+    hideQuestion(this);
+  });
+});
